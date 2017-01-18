22e32c6f80ecce60384c330820cb73aba4db87b1
WindFireWheel <fr440305@gmail.com>
2017-01-17 16:12:30 -0800

    Update socket/main, adding argument check

--- socketprog/main.c
+++ socketprog/main.c
@@ -1,8 +1,13 @@
 #include <sys/socket.h>
-
 #include <stdio.h>
-
 #include <stdlib.h>
 
 int main (int ArgN, char **ArgList) {
+	int i;
+	printf ("%d\n", i);
+	printf ("%d\n", ArgN);
+	for (i = 0; i < ArgN; i++) {
+		printf ("%s;\n", ArgList[i]);
+	}
+	return 0;
 }
