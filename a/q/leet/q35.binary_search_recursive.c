bc9025b313343b3aa8de4d09030ae68fd12fd6e0
Huoyu <fr440305@gmail.com>
2018-06-01 05:21:11 -0700

    a/q/35.binary_search_recursive :: update

--- a/q/leet/q35.binary_search_recursive.c
+++ a/q/leet/q35.binary_search_recursive.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
 
-int find(int* arr, int off, int len, int tar) {
-	/* Note that len cannot be zero. */
-	int mid = off + len/2;
-	int odd = len % 2;
+/* find() - Find or show the relevant position of target in an array.
+ * - arr    : The array.
+ * - lo, hi : The end-points.
+ * - tar    : The target. */
+int find(int* arr, int lo, int hi, int tar) {
+	int mid = (lo + hi) / 2;
 
-	printf("find %d from arr[off == %d, len == %d]\n", tar, off, len);
-
-	if (len == 1) /* base case */
-		return off + (tar > arr[off]);
+	if (arr[mid] == tar) /* base case i - hit */
+		return mid;
+	else if (lo > hi) /* base case ii - over */
+		return lo;
+	else if (arr[mid] < tar)
+		return find(arr, mid + 1, hi, tar);
 	else
-		if (arr[mid] == tar)
-			return mid;
-		else if (arr[mid] < tar)
-			return find(arr, mid + odd, len/2, tar);
-		else
-			return find(arr, off, len/2, tar);
+		return find(arr, lo, mid - 1, tar);
 }
 
 int searchInsert(int* nums, int numsSize, int target) {
-	if (nums == 0)
-		return 0;
-	else
-		return find(nums, 0, numsSize, target);
+	return (numsSize == 0) ? 0 : find(nums, 0, numsSize - 1, target);
 }
 
 
 int main() {
 	int nums[5] = {2, 7, 8, 9, 10};
 
-	/* expect: 2 */
-	printf("%d", searchInsert(nums, 5, 9));
+	printf("== %d", searchInsert(nums, 4, 12));
+
 	return 0;
 }
 
